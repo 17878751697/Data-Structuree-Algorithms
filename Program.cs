72ccf475e07952d88d419ddc6431e6ea4a54b9ef
17878751697 <2382931200@qq.com>
2019-11-26 12:02:02 +0800

    模块 helloword 的初始签入

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloword
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
